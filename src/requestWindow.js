990e4975e1796be54b58ccc30c4d48de857b2e87
unknown <sharjeel50@hotmail.co.uk>
2019-03-07 19:30:58 +0000

    Database working and complete with HTML Table update

--- src/requestWindow.js
+++ src/requestWindow.js
@@ -1,47 +1,56 @@
+const form = document.getElementById('requestButton')
 
- const form = document.getElementById('requestButton')
+if (form) {
+    form.addEventListener('click', function () { // Add event listener on button
 
- console.log("Working");
+        // ---------------------------------------------  Validation --------------------------------------------------------
 
- if(form){
-   form.addEventListener('click', function () { // Add event listener on button
-
-      console.log("Wokring");
-
-  		var mysql = require("mysql");
-
-  		var connection = mysql.createConnection({ // Make the connection to the database
-  			host: "localhost",
-  			user: "root",
-  			password: null,
-  			database: "requestTable"
-  		});
-
-  		connection.connect((err) => { // Check if conenction is made or not
-  			if (err) {
-  				return console.log(" Connection Failed " + err);
-  			}
-  			console.log("Connected");
-  		})
-
-      // Make the SQL queries here
-      const bloodType = document.getElementById("bloodType").value;
-      const requestType = document.getElementById("requestType").value;
-      const Notice = document.getElementById("Notice").value;
-
-      $queryString = "INSERT INTO `requeststable`(`request_type`, `blood_type`, `notice`, `date`) VALUES ('" + requestType + "', '" + bloodType + "', '" + Notice + "', CURRENT_TIMESTAMP)";
-
-      connection.query($queryString, (err, rows, fields) => {
-        if(err){
-          return console.log("Error with Query");
+        if (document.forms["requestForm"]["reqType"].value == "") {
+            alert("Enter a Request Type")
+            return false;
         }
-        console.log(rows);
-      })
 
-  		connection.end(() => {
-  			console.log("Connection Closed");
-  		})
-  	})
- }else{
-   console.log("Button not working");
- }
+        if (document.forms["requestForm"]["noticeType"].value == "") {
+            alert("Enter a Notice Type")
+            return false;
+        }
+
+        // ---------------------------------------------  Database --------------------------------------------------------
+
+		var mysql = require("mysql");
+		var connection = mysql.createConnection({ // Make the connection to the database
+			host: "localhost",
+			user: "root",
+			password: null,
+			database: "requestTable"
+		});
+
+		connection.connect((err) => { // Check if conenction is made or not
+			if (err) {
+				return console.log(" Connection Failed " + err);
+			}
+			console.log("Connected");
+		})
+
+		// Make the SQL queries here
+		const bloodType = document.getElementById("bloodType").value;
+		const requestType = document.getElementById("requestType").value;
+		const Notice = document.getElementById("Notice").value;
+
+        $queryString = "INSERT INTO `requeststable`(`request_type`, `blood_type`, `notice`, `date`) VALUES ('" + requestType + "', '" + bloodType + "', '" + Notice + "', CURDATE());";
+
+		connection.query($queryString, (err, rows, fields) => {
+			if (err) {
+				return console.log("Error with Query");
+			}
+			console.log(rows);
+			console.log(fields);
+		})
+
+		connection.end(() => {
+			console.log("Connection Closed");
+		})
+	})
+} else {
+	console.log("Button not working");
+}
